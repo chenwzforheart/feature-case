3228118edf52363529f846f847857520adff390c
wenzheng.chen <wenzheng.chen@baozun.com>
2019-04-15 12:53:54 +0800

    scala first

---
+++ src/src/main/java/JavaMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by csh9016 on 2019/4/15.
+ */
+public class JavaMain {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/src/main/scala/ScalaApp.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by csh9016 on 2019/4/15.
+  */
+class ScalaApp {
+  def main(args: Array[String]): Unit = {
+    println("hello scala")
+  }
+}
