00d6aacbedb806b4608ab897b282d56754b7922a
wenzheng.chen <wenzheng.chen@baozun.com>
2019-12-06 17:12:58 +0800

    在线用户，打印header

--- login/src/main/java/com/example/login/HomeController.java
+++ login/src/main/java/com/example/login/HomeController.java
@@ -1,12 +1,16 @@
 package com.example.login;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Set;
 
 /**
  * @author csh9016
@@ -17,8 +21,21 @@ import javax.servlet.http.HttpSession;
 @RestController
 public class HomeController {
 
+    @Autowired
+    private RedisTemplate<Object,Object> redisTemplate;
+
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public void login(String username, String password, HttpSession httpSession) {
-        log.info("username:{},password:{},sessionId:{}",username,password,httpSession.getId());
+    public void login(@RequestBody LoginInfo loginInfo, HttpServletRequest request, HttpSession httpSession) {
+        String remoteAddr = request.getRemoteAddr();
+        httpSession.setAttribute("loginFlag",true);
+        httpSession.setAttribute("loginIP",remoteAddr);
+        httpSession.setAttribute("loginUser",loginInfo.getUsername());
+        log.info("username:{},password:{},sessionId:{},ip:{}",loginInfo.getUsername(),loginInfo.getPassword(),httpSession.getId(),remoteAddr);
+    }
+
+    @RequestMapping(value = "/siteInfo",method = RequestMethod.GET)
+    public String siteInfo() {
+        Set keys = redisTemplate.keys("spring:session:sessions:expires:*");
+        return String.format("在线人数:%d,登录用户:%d",keys.size(),0);
     }
 }
---
+++ login/src/main/java/com/example/login/LoginInfo.java
@@ -0,0 +1,14 @@
+package com.example.login;
+
+import lombok.Data;
+
+/**
+ * @author csh9016
+ * @date 2019/12/6
+ */
+@Data
+public class LoginInfo {
+
+    private String username;
+    private String password;
+}
