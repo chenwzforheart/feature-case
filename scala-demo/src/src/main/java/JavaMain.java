a1ff0422cac19e4d2e7a9f69cb2f11ada3bcedf2
wenzheng.chen <wenzheng.chen@baozun.com>
2020-06-01 03:30:24 +0800

    清理多余模块-1

--- scala-demo/src/src/main/java/JavaMain.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by csh9016 on 2019/4/15.
- */
-public class JavaMain {
-
-    public static void main(String[] args) {
-
-    }
-}
--- scala-demo/src/src/main/scala/ScalaApp.scala
+++
@@ -1,8 +0,0 @@
-/**
-  * Created by csh9016 on 2019/4/15.
-  */
-class ScalaApp {
-  def main(args: Array[String]): Unit = {
-    println("hello scala")
-  }
-}
